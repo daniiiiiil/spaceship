43e8b7c9d892a9a750a0702105e11364432d7a35
daniiiiiil <93081867+daniiiiiil@users.noreply.github.com>
2023-03-02 22:39:32 +0300

    Update ino.py

--- ino.py
+++ ino.py
@@ -4,7 +4,7 @@ class Ino(pygame.sprite.Sprite):
     def __init__(self, screen):
         super(Ino, self).__init__()
         self.screen = screen
-        self.image = pygame.image.load('../game/alien.png')
+        self.image = pygame.image.load('game/alien.png')
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
